dbac116ab080e373d8cf103005ec551db06780b2
Tomás Candeias <tomascandeias@ua.pt>
2019-11-27 11:19:56 +0000

    final version

--- FP/TypeRacer Game/typeracer.py
+++ FP/TypeRacer Game/typeracer.py
@@ -8,8 +8,12 @@ def fileSetUp(fname):
           s += line
     return s
 
+def spacer():
+    print(50*"-")
+    print()
 
 def race(phrase):
+    spacer()
     print(phrase)
 
     # SAVE PHRASE INTO A LIST OF WORDS
@@ -19,58 +23,57 @@ def race(phrase):
     start = time.time()
     wrotedWords = 0
     for s in wordLst:
-        wrotedWords += askStr(s)
+        wrotedWords = askStr(s, wrotedWords)
+
+    duration = float(time.time() - start) #total time
 
     # DISPLAY THE FINAL RESULTS
-    duration = float(time.time() - start)
+    print("\n{:^100}".format("--->FINAL RESULTS<---"))
+
     print("Time elapsed: {:.2f}s".format(duration))  # duration in seconds
 
-    # wrotedWords  --- 100%
-    # len(wordLst) --- x
-    acc = (len(wordLst) * 100) / wrotedWords
-    print("Accuracy: {:.1f}%".format(acc))  # accuracy %
+    acc = ((wrotedWords * 100) / len(wordLst)) -100
+    print("Accuracy: {:.1f}%".format(100-acc))  # accuracy %
 
     wps = len(wordLst) / float(duration)
     print("WPS: {:.2f} words per second".format(wps))   # WPS = words per second
+
     wpm = wps*60
     print("WPM: {:.2f} words per minute".format(wpm))    # WPM = words per minute
 
 
-def askStr(palavra):
-    wroted_words = 1
+def askStr(palavra, wroted_words):
+
     while True:
         wroted_input = str(input("--> {} : ".format(palavra)))  # --> {palavra} :
+        wroted_words += 1
 
         if wroted_input != palavra:  # TYPE ERROR
             while True:
-                print("\033[A                             \033[A")  # clear last output line
                 wroted_input = str(input("-->False, {} : ".format(palavra)))
                 wroted_words += 1
 
                 if wroted_input == palavra:
-                    print("\033[A                             \033[A")  # clear last output line
                     break
             break  # exit while True
         else:  # wroted_input == palavra
-            print("\033[A                             \033[A")  # clear last output line
-            wroted_words += 1
+            break
 
         break  # exit while True
 
-    if wroted_words == 2:
-        return wroted_words - 1
-    else:
-        return wroted_words
+    return wroted_words
 
 
 def main():
     print("--->WELCOME TO THE TYPERACER<--")
     while True:
-        fname = str(input("File name: "))
-        if (fname[len(fname)-4:len(fname)]) == ".txt":
-            text = fileSetUp(fname)
-            break
-
+        try:
+            fname = str(input("File name: "))
+            if (fname[len(fname)-4:len(fname)]) == ".txt":
+                text = fileSetUp(fname)
+                break
+        except FileNotFoundError:
+            print("File not found, please enter a valid file name")
     race(text)
 
 main()
